d0b20d8e35ed5f7b3c3b628686c67ac9ceae9bfe
cleo6920 <althea12830@gmail.com>
2025-08-20 10:13:30 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,4 @@
 // api/ping.js
 module.exports = (req, res) => {
-  res.status(200).json({ ok: true, message: "pong", time: Date.now() });
+  res.status(200).json({ ok: true, message: 'pong', time: Date.now() });
 };
